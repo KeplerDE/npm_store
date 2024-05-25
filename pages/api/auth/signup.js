2f4981fba5c736e1ca8605b5c5971b826b13eeda
keple <keplerde@gmail.com>
2024-05-25 08:12:32 +0200

    model_User, token

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,23 +1,43 @@
 import nc from 'next-connect';
-import { connectDb } from '../../../utils/db'; 
-
+import { validateEmail } from '../../../utils/validation';
+import { connectDb } from '../../../utils/db';
+import User from '../../../models/User';
+import bcrypt from 'bcryptjs';
+import { createActivationToken } from '../../../utils/tokens';
 
 const handler = nc();
 
 handler.post(async (req, res) => {
   try {
-    await connectDb(); // Подключение к базе данных
+    await connectDb();
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
       return res.status(400).json({ message: "Please fill in all fields." });
     }
 
-    // Логика для создания нового пользователя в базе данных
-    // Пример:
-    // const newUser = new User({ name, email, password });
-    // await newUser.save();
+    if (!validateEmail(email)) {
+      return res.status(400).json({ message: "Invalid email." });
+    }
 
-    res.status(200).json({ message: "User created successfully." });
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "This email already exists." });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters." });
+    }
+
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: cryptedPassword });
+    const addedUser = await newUser.save();
+
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
+
+    console.log(activation_token);
+    res.send(activation_token);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
