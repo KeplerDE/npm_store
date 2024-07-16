4af10f7f8a6e06aadbb299ee2f701096eb6b567a
keple <keplerde@gmail.com>
2024-07-16 04:22:58 +0200

    home main_structure

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,18 @@ import styles from '../styles/Home.module.scss';
 import Header from '../components/Header';
 import Footer from '@/components/Footer';
 import { useSession } from 'next-auth/react';
+import Main from '@/components/home/main'; 
 
 export default function Home({ country }) {
   const { data: session } = useSession();
   return (
     <div>
-      <Header country={country}/>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Header country={country} />
+      <main className={styles.home}>
+        <div className={styles.container}>
+          <Main /> 
+        </div>
+
       </main>
       {session ? "You are logged in" : "You are not logged in"}
       <Footer country={country} />
